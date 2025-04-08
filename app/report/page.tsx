51694c7e515dce902632589fdbd9288a34e68761
STAREBUG <shivharekartik13@gmail.com>
2025-04-08 18:15:45 +0530

    Issues Component

---
+++ app/report/page.tsx
@@ -0,0 +1,9 @@
+import ReportIssueForm from "@/app/components/ReportIssueForm";
+
+export default function ReportPage() {
+  return (
+    <main className="min-h-screen p-8 bg-zinc-950 text-white">
+      <ReportIssueForm />
+    </main>
+  );
+}
